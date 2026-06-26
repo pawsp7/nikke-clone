4985bf8ac9b578535502c432c938b569b0459fdc
Hailey Smolak <pawsp7@gmail.com>
2026-06-26 15:55:30 -0600

    created and setup dynamic squad routing

---
+++ nikke-clone/data/squads.ts
@@ -0,0 +1,7 @@
+export const squads: Record<string, string[]> = {
+  "01": ["Dorothy", "Liberalio", "Anne: Miracle", "Little Mermaid", "Diesel"],
+  "02": ["Crown", "Little Mermaid", "Dorothy", "Liberalio", "Anis: Star"],
+  "03": ["Zwei", "Mint", "Prika", "Miranda", "Liter"],
+  "04": [],
+  "05": [],
+};
